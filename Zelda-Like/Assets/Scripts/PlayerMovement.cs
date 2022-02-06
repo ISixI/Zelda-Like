bfcd3ab96b4b61b930b27c19ad69a02386fe5c99
ISixI <j.boxleyx@gmail.com>
2022-02-06 06:58:53 -0600

    End of Chapter 3

--- Zelda-Like/Assets/Scripts/PlayerMovement.cs
+++ Zelda-Like/Assets/Scripts/PlayerMovement.cs
@@ -7,10 +7,12 @@ public class PlayerMovement : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidBody;
     private Vector3 change;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidBody = GetComponent<Rigidbody2D>();
     }
 
@@ -20,15 +22,25 @@ public class PlayerMovement : MonoBehaviour
         change = Vector3.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-        if(change != Vector3.zero)
-        {
-            MoveCharacter();
-        }
+       UpdateAnimationAndMove();
     }
 
+    void UpdateAnimationAndMove()
+    {
+         if(change != Vector3.zero)
+        {
+            MoveCharacter();
+            animator.SetFloat("moveX", change.x);
+            animator.SetFloat("moveY", change.y);
+            animator.SetBool("moving", true);
+            }else{
+                animator.SetBool("moving",false);
+            }
     void MoveCharacter()
     {
         myRigidBody.MovePosition(transform.position + change * speed * Time.deltaTime);
 
     }
+    }
+
 }
